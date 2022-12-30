e9718b5c0bafb276a2bfcb58e0fa2fcedf2200bc
justkod <luckoding@gmail.com>
2022-12-30 16:41:36 +0900

    :fire: 221230 - add : User CRUD 작업

--- src/apis/users/users.resolver.ts
+++ src/apis/users/users.resolver.ts
@@ -1,4 +1,7 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateUserInput } from './dto/createUser.input';
+import { UpdateUserInput } from './dto/updateUser.input';
+import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
 @Resolver()
@@ -6,4 +9,34 @@ export class UsersResolver {
   constructor(
     private readonly usersService: UsersService, //
   ) {}
+
+  @Query(() => [User], { description: '회원 조회' })
+  async fetchAllUsers() {
+    return this.usersService.findAll();
+  }
+
+  @Mutation(() => User, { description: '회원가입' })
+  async createUser(
+    @Args('createUserInput', { description: '회원 가입 정보 입력' })
+    createUserInput: CreateUserInput,
+  ) {
+    return this.usersService.create(createUserInput);
+  }
+
+  @Mutation(() => User, { description: '회원 정보 수정' })
+  async updateUser(
+    @Args('updateUserInput', { description: '수정할 회원 정보' })
+    updateUserInput: UpdateUserInput,
+  ) {
+    return this.usersService.update(updateUserInput);
+  }
+
+  @Mutation(() => Boolean, {
+    description: 'Return : deletedAt(유저 정보 삭제된 시간)',
+  })
+  deleteUser(
+    @Args('email', { description: '회원의 계정(메일주소)' }) email: string, //
+  ) {
+    return this.usersService.delete({ email });
+  }
 }
--- src/apis/users/users.service.ts
+++ src/apis/users/users.service.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async findAll() {
+    const result = await this.usersRepository.find();
+    return result;
+  }
+
+  async create(createUserInput) {
+    const user = await this.usersRepository.findOne({
+      where: { email: createUserInput.email },
+    });
+    if (user) {
+      throw new ConflictException('이미 등록된 이메일입니다.');
+    }
+
+    const salt = process.env.BCRYPT_USER_SALT;
+
+    const hashedPassword = await bcrypt.hash(
+      createUserInput.password,
+      Number(salt),
+    );
+    const result = await this.usersRepository.save({
+      ...createUserInput,
+      password: hashedPassword,
+    });
+    return result;
+  }
+
+  async update(updateUserInput) {
+    const user = await this.usersRepository.findOne({
+      where: { email: updateUserInput.email },
+    });
+    if (!user)
+      throw new ConflictException(
+        '해당하는 회원 정보가 없습니다. 이메일을 확인해주세요.',
+      );
+    const salt = process.env.BCRYPT_USER_SALT;
+    const hashedPassword = await bcrypt.hash(
+      updateUserInput.password,
+      Number(salt),
+    );
+
+    const result = await this.usersRepository.save({
+      ...user,
+      ...updateUserInput,
+      password: hashedPassword,
+    });
+    return result;
+  }
+
+  async delete({ email }) {
+    const result = await this.usersRepository.softDelete({ email });
+    return result.affected ? true : false;
+  }
+}
