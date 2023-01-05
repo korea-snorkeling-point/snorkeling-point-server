80c86a1a0698b5a0149acf084842b965ed167d5d
justkod <luckoding@gmail.com>
2023-01-05 21:21:55 +0900

    :fire: 230105 - add : login 추가

--- src/apis/auths/auths.resolver.ts
+++ src/apis/auths/auths.resolver.ts
@@ -1,9 +1,36 @@
-import { Resolver } from '@nestjs/graphql';
+import { UnprocessableEntityException } from '@nestjs/common';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { UsersService } from '../users/users.service';
 import { AuthsService } from './auths.service';
+import { IContext } from 'src/commons/type/context';
+import * as bcrypt from 'bcrypt';
 
 @Resolver()
 export class AuthsResolver {
   constructor(
+    private readonly usersService: UsersService, //
     private readonly authsService: AuthsService, //
   ) {}
+
+  @Mutation(() => String)
+  async login(
+    @Args('email', { description: '로그인 할 회원 이메일 정보' }) email: string, //
+    @Args('password', { description: '로그인 할 회원 비밀번호' })
+    password: string,
+    @Args('context') Context: IContext,
+  ) {
+    const user = await this.usersService.findOne({ email });
+    if (!user) throw new UnprocessableEntityException('이메일이 없습니다.');
+
+    const isAuth = await bcrypt.compare(password, user.password);
+    if (!isAuth) throw new UnprocessableEntityException('암호가 틀렸습니다.');
+
+    this.authsService.setRefreshToken({
+      user,
+      res: Context.res,
+      req: Context.req,
+    });
+
+    return this.authsService.getAccessToken({ user });
+  }
 }
--- src/apis/auths/auths.service.ts
+++ src/apis/auths/auths.service.ts
@@ -1,7 +1,41 @@
 import { Injectable } from '@nestjs/common';
-
+import { JwtService } from '@nestjs/jwt';
 /**
  * Auth Service
  */
 @Injectable()
-export class AuthsService {}
+export class AuthsService {
+  constructor(
+    private readonly jwtService: JwtService, //
+  ) {}
+
+  getAccessToken({ user }) {
+    return this.jwtService.sign(
+      { email: user.email, sub: user.id },
+      { secret: 'myAccessKey', expiresIn: '1h' },
+    );
+  }
+
+  setRefreshToken({ user, res, req }) {
+    const refreshToken = this.jwtService.sign(
+      { email: user.email, sub: user.id },
+      { secret: process.env.JWT_REFRESH_SECRET, expiresIn: '2w' },
+    );
+
+    const allowedOrigins = process.env.CORS_URLS.split(', ');
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
+    );
+    res.setHeader(
+      'Set-Cookie',
+      `refreshToken=${refreshToken}; path=/; domain=.snkserver.shop; SameSite=None; Secure; httpOnly;`,
+    );
+  }
+}
